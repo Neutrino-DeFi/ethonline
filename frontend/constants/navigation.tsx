6f6e0f929c52b1a05ecd47300df18a447f3ff3c4
Arunava-Barua <rahulbarua31@gmail.com>
2025-10-22 19:23:07 +0530

    Neutrade to Neutrino

--- frontend/constants/navigation.tsx
+++ frontend/constants/navigation.tsx
@@ -1,50 +1,50 @@
 export const navigation = [
-    {
-        id: "0",
-        title: "Home",
-        icon: "home",
-        url: "/",
-    },
-    {
-        id: "1",
-        title: "My assets",
-        icon: "wallet",
-        url: "/my-assets",
-    },
-    {
-        id: "2",
-        title: "Notification",
-        icon: "notification",
-        url: "/notification",
-    },
-    {
-        id: "3",
-        title: "Trade",
-        icon: "trade",
-        url: "/trade",
-    },
-    {
-        id: "4",
-        title: "Neutrade AI",
-        icon: "star-plus",
-        url: "/chat",
-    },
-    {
-        id: "5",
-        title: "Strategies",
-        icon: "settings",
-        url: "/strategies",
-    },
-    {
-        id: "6",
-        title: "Premium",
-        icon: "star",
-        url: "/pricing",
-    },
-    {
-        id: "7",
-        title: "Settings",
-        icon: "support",
-        url: "/settings",
-    },
+  {
+    id: "0",
+    title: "Home",
+    icon: "home",
+    url: "/",
+  },
+  {
+    id: "1",
+    title: "My assets",
+    icon: "wallet",
+    url: "/my-assets",
+  },
+  {
+    id: "2",
+    title: "Notification",
+    icon: "notification",
+    url: "/notification",
+  },
+  {
+    id: "3",
+    title: "Trade",
+    icon: "trade",
+    url: "/trade",
+  },
+  {
+    id: "4",
+    title: "Neutrino AI",
+    icon: "star-plus",
+    url: "/chat",
+  },
+  {
+    id: "5",
+    title: "Strategies",
+    icon: "settings",
+    url: "/strategies",
+  },
+  {
+    id: "6",
+    title: "Premium",
+    icon: "star",
+    url: "/pricing",
+  },
+  {
+    id: "7",
+    title: "Settings",
+    icon: "support",
+    url: "/settings",
+  },
 ];
